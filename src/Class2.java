80e785e2359b91a01f94a178e49631b026d7a25c
JAEHYEON PARK <pj6577@naver.com>
2022-03-11 20:37:06 +0900

    Claas 2 variable

---
+++ src/Class2.java
@@ -0,0 +1,26 @@
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    //문제 
+	    //이름 : 박재현
+	    //나이 : 23
+	    //키  :   178.7
+	    //아이큐 120
+	    //등급 : B
+	    
+	    String name = "박재현";
+	    int age = 23;
+	    double key = 178.7;
+	    int iq = 120;
+	    char 등급 = 'B';
+	    System.out.println("이름\t:" + name);
+	    System.out.println("나이\t:" +age);
+	    System.out.println("아이큐\t:" + iq);
+	    System.out.println("등급\t:" + 등급);
+	    
+	    
+	}
+
+}
