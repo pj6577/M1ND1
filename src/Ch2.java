42a398a16dfdbd2299f45d41a6218fc8bec34a0d
JAEHYEON PARK <pj6577@naver.com>
2022-03-11 17:41:39 +0900

    variable

--- src/Ch2.java
+++ src/Ch2.java
@@ -9,6 +9,7 @@ public class Ch2 {
 		System.out.println(x-y); //»¬¼À
 		System.out.println(x*y); //°ö¼À
 		System.out.println(x/y); //³ª´°¼À
+		
 		int year = 0, age = 22 ;
 		year = age + 2000 ;
 		System.out.println(year);
