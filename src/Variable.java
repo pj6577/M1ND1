c2b39ce2d8f353e8f105c7656da73817dcc6f946
JAEHYEON PARK <pj6577@naver.com>
2022-03-11 18:04:04 +0900

    EX

---
+++ src/Variable.java
@@ -0,0 +1,21 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // º¯¼ö ½Ç½À ÇØº¸±â
+		int x = 4, y = 2; 
+		System.out.println(x+y); //µ¡¼À
+		System.out.println(x-y); //»¬¼À
+		System.out.println(x*y); //°õ¼À
+		System.out.println(x/y); //³ª´°¼À
+		System.out.println(x);
+		System.out.println(y);
+		int year = 0, age = 22 ;
+		year = age + 2000 ;
+		System.out.println(year);
+		age = age + 1 ;
+		System.out.println(age) ;
+	}
+
+}
